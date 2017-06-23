107a86ddd8511a7d131de73113d7d9c7c45d7eda
Defong <defongitstep@gmail.com>
2017-06-23 11:11:41 +0300

    Update App.h

--- App.h
+++ App.h
@@ -8,10 +8,12 @@ private:
 	HANDLE hConsole;
 	BattleShip * GameApp;
 	int shift[2]; // Distance between player fields
+	bool pause;
 public:
 	App();
 	~App();
 
+	void Pause();
 	void Draw(bool mode); // Output of game fields and their contents (mode - Hide enemy field or not)
 	void Update(); // Initialize and run all game mechanics
 };
